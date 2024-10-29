b8ce35d2ef2af42a20ec4d51067f86cf24ac665e
cstahly <cstahly@gmail.com>
2024-10-29 15:56:08 +0000

    fix web service worker

--- client/web/firebase-messaging-sw.js
+++ client/web/firebase-messaging-sw.js
@@ -1,3 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging-compat.js');
+
 firebase.initializeApp({
     "projectId": "borkbook-35422",
     "appId": "1:1007477594478:web:45e7aa9e9c48255a0cc97f",
@@ -7,4 +10,18 @@ firebase.initializeApp({
     "messagingSenderId": "1007477594478",
     "measurementId": "G-WFN7R4R7N8"
   });
-  
+
+// Retrieve an instance of Firebase Messaging to handle background messages
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png', // Set an appropriate icon for your app
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
